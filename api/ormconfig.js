3fc646785ac204c22c6069c617e0be94d15213af
demetoir <wnsqlehlswk@naver.com>
2020-06-06 15:25:24 +0900

    chore: typeorm migration setting

---
+++ api/ormconfig.js
@@ -0,0 +1,27 @@
+const node_env = process.env.NODE_ENV;
+
+// todo 여기에서 node env 에 따라 다르게 나오도록 만들기
+// 		production, dev, test 다르게...
+
+module.exports = {
+	type: "mysql",
+	host: "localhost",
+	port: 3306,
+	username: "user",
+	password: "password",
+	database: "expression_cloud",
+	synchronize: false,
+	logging: false,
+	entities: [
+		"src/model/entity/**/*.ts"
+	],
+	migrations: [
+		"src/model/migration/**/*.ts"
+	],
+	cli: {
+		"migrationsDir": "src/model/migration"
+	},
+	subscribers: [
+		"src/model/subscriber/**/*.ts"
+	]
+}
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,7 +2,7 @@ import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
-import {User} from "./entities/user.entity"
+import {User} from "./model/entity/user.entity"
 
 const entities = [User];
 
