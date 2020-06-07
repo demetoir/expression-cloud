c70a5ab727ad4b6c8cef85f997bef48470b2ff6d
demetoir <wnsqlehlswk@naver.com>
2020-06-07 17:25:45 +0900

    chore: node_env 별 typeorm option 로드하도록 설정

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -1,28 +1,69 @@
-const node_env = process.env.NODE_ENV;
-
-// todo 여기에서 node env 에 따라 다르게 나오도록 만들기
-// 		production, dev, test 다르게...
-
-module.exports = {
-	type: "mysql",
-	host: "localhost",
+const dev = {
+	type: 'mysql',
+	host: 'localhost',
 	port: 3306,
-	username: "user",
-	password: "password",
-	database: "expression_cloud",
+	username: 'user',
+	password: 'password',
+	database: 'expression_cloud',
 	synchronize: false,
 	logging: false,
 	bigNumberStrings: false,
-	entities: [
-		"src/model/entity/**/*.ts"
-	],
-	migrations: [
-		"src/model/migration/**/*.ts"
-	],
+	entities: ['src/model/entity/**/*.entity.ts'],
+	migrations: ['src/model/migration/**/*.ts'],
 	cli: {
-		"migrationsDir": "src/model/migration"
+		migrationsDir: 'src/model/migration',
 	},
-	subscribers: [
-		"src/model/subscriber/**/*.ts"
-	]
+	subscribers: ['src/model/subscriber/**/*.ts'],
+};
+
+const prod = {
+	type: 'mysql',
+	host: 'localhost',
+	port: 3306,
+	username: 'user',
+	password: 'password',
+	database: 'expression_cloud',
+	synchronize: false,
+	logging: false,
+	bigNumberStrings: false,
+	entities: ['src/model/entity/**/*.entity.ts'],
+	migrations: ['src/model/migration/**/*.ts'],
+	cli: {
+		migrationsDir: 'src/model/migration',
+	},
+	subscribers: ['src/model/subscriber/**/*.ts'],
+};
+
+const test = {
+	type: 'mysql',
+	host: 'localhost',
+	port: 3306,
+	username: 'user',
+	password: 'password',
+	database: 'expression_cloud',
+	synchronize: false,
+	logging: false,
+	bigNumberStrings: false,
+	entities: ['src/model/entity/**/*.entity.ts'],
+	migrations: ['src/model/migration/**/*.ts'],
+	cli: {
+		migrationsDir: 'src/model/migration',
+	},
+	subscribers: ['src/model/subscriber/**/*.ts'],
+};
+
+let config;
+const node_env = process.env.NODE_ENV || 'dev';
+if (node_env === 'dev') {
+	config = dev;
+} else if (node_env === 'test') {
+	config = test;
+} else if (node_env === 'prod') {
+	config = prod;
+} else {
+	throw new Error(`node_env expect one of dev, test, prod, but ${node_env}`);
 }
+
+console.log(`load typeorm config as ${node_env}`);
+
+module.exports = config;
