ba07f2985b6732936b17b438371c225fd759f611
demetoir <wnsqlehlswk@naver.com>
2020-06-09 22:49:11 +0900

    chore: import ormconfig.js option to app module

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -2,27 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserEntity } from '../model/entity/user.entity';
-import { RoleEntity } from '../model/entity/role.entity';
-import { UserSettingEntity } from '../model/entity/userSetting.entity';
-
-const entities = [UserEntity, RoleEntity, UserSettingEntity];
+import ormconfig from "../../ormconfig.js"
 
 // todo .env 에서 설정 읽도록 만들기
 //    synchronize 는 test 에서만 되도록 true 되도록 만들기
 //    아니면 그냥 typeorm 의 외부 config를 사용해서 는거 고려하기
+
 @Module({
 	imports: [
-		TypeOrmModule.forRoot({
-			type: 'mysql',
-			host: 'localhost',
-			port: 3306,
-			username: 'root',
-			password: 'root',
-			database: 'test',
-			entities: entities,
-			synchronize: true,
-		}),
+		TypeOrmModule.forRoot(ormconfig),
 	],
 	controllers: [AppController],
 	providers: [AppService],
