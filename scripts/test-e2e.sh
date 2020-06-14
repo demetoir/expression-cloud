f2201b768f76535d06a107ec1da33bd6f93b10da
demetoir <wnsqlehlswk@naver.com>
2020-06-14 15:06:13 +0900

    chore: split unit test and e2e test script

---
+++ scripts/test-e2e.sh
@@ -0,0 +1,12 @@
+pwd
+
+# goto root directory
+
+cd ./api
+yarn test:e2e
+cd ..
+
+
+cd ./app
+yarn test:e2e
+cd ..
--- scripts/test-unit.sh
+++ scripts/test-unit.sh
@@ -4,12 +4,9 @@ pwd
 
 cd ./api
 yarn test
-yarn test:e2e
 cd ..
 
 
 cd ./app
 yarn test:unit
-yarn test:e2e
-
 cd ..
