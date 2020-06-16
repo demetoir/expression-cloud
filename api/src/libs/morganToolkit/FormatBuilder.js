02e832d7bd1b59e7b74692d9b7d674c52683e985
demetoir <wnsqlehlswk@naver.com>
2020-06-16 22:58:31 +0900

    fix: typo

--- api/src/libs/morganToolkit/FormatBuilder.js
+++ api/src/libs/morganToolkit/FormatBuilder.js
@@ -1,5 +1,5 @@
 module.exports.FormatBuilder = class FormatBuilder {
 	static build(format, tokens) {
-		return `${tokens.remoteAddr} - ${tokens.remoteUser} [${tokens.date}] "${tokens.methodToken} ${tokens.status} ${tokens.url} HTTP/${tokens.httpVersion}" ${tokens.responseTime} ${tokens.contentLength}" - "${tokens.referrer} ${tokens.userAgent}"`;
+		return `${tokens.remoteAddr} - ${tokens.remoteUser} [${tokens.date}] "${tokens.method} ${tokens.status} ${tokens.url} HTTP/${tokens.httpVersion}" ${tokens.responseTime} ${tokens.contentLength}" - "${tokens.referrer} ${tokens.userAgent}"`;
 	}
 };
