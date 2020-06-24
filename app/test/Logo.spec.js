c89907edf3d9f14e10bc5ff3721c105f543480b4
demetoir <wnsqlehlswk@naver.com>
2020-06-24 22:06:19 +0900

    chore: test todo bot

--- app/test/Logo.spec.js
+++ app/test/Logo.spec.js
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils'
 import Logo from '@/components/Logo.vue'
 
+// todo this is test
 describe('Logo', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Logo)
