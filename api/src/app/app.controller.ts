c5dcd26699d9b0b7ce16f94678065bf083638c68
demetoir <wnsqlehlswk@naver.com>
2020-06-25 21:42:19 +0900

    tutorial: add swagger sample

--- api/src/app/app.controller.ts
+++ api/src/app/app.controller.ts
@@ -1,7 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { logger } from '../libs/winstonToolkit';
+import { ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+class readDto {
+	@ApiProperty()
+	name: string;
+
+	@ApiProperty({
+		description: 'The age of a cat',
+		minimum: 1,
+		default: 1,
+		type: Number,
+	})
+	age: number;
+
+	@ApiProperty()
+	breed: string;
+}
+
+// todo: swagger auth 설정하는거 추가
+@ApiTags('hello')
 @Controller()
 export class AppController {
 	private logger: any;
@@ -11,9 +30,21 @@ export class AppController {
 		this.logger.info('create');
 	}
 
-	@Get('/')
+	@Get('/hello')
 	getHello(): string {
 		this.logger.info('here');
 		return this.appService.getHello();
+	}
+
+	@ApiResponse({
+		status: 201,
+		description: 'The record has been successfully created.',
+	})
+	@ApiResponse({ status: 403, description: 'Forbidden.' })
+	@Post('/hello')
+	postHello(@Body() dto: readDto, @Query() query: any): string {
+		this.logger.info(`put hello ${dto} ${query}`);
+
+		return dto.toString();
 	}
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { CustomMorgan } from './middlewares/loggerMiddlewares';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.use(CustomMorgan());
 
+	initSwagger(app);
+
 	await app.listen(3000);
 }
 
 bootstrap();
+
+function initSwagger(app) {
+	const options = new DocumentBuilder()
+		.setTitle('Expression Cloud')
+		.setDescription('Expression Cloud')
+		.setVersion('1.0')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, options);
+
+	SwaggerModule.setup('/api/v1', app, document);
+}
