081b1b0bf32b054b89f5c7a933a098130d0833bd
demetoir <wnsqlehlswk@naver.com>
2020-06-30 22:55:00 +0900

    chore: add todo

--- api/src/model/entity/projectSetting.entity.ts
+++ api/src/model/entity/projectSetting.entity.ts
@@ -5,6 +5,8 @@ import { AbstractBaseEntity } from './abstractBase.entity';
 @Entity({ name: 'project_settings' })
 export class ProjectSettingEntity extends AbstractBaseEntity {
 	// todo project setting 컬럼 추가하기
+	// `is_public`   tinyint(1)         NOT NULL DEFAULT false,
+	//     `is_locked`   tinyint(1)         NOT NULL DEFAULT false,
 
 	@OneToOne(() => ProjectEntity, (project) => project.setting)
 	@JoinColumn({ name: 'project_id', referencedColumnName: 'id' })
