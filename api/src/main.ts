a2b1aff036aa217bbd26d667187e01af2b459b12
demetoir <wnsqlehlswk@naver.com>
2020-07-06 21:55:47 +0900

    feature: add basic security middleware and option for nest.js

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { CustomMorgan } from './middlewares/loggerMiddlewares';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import * as expectCt from 'expect-ct';
+import * as rateLimit from 'express-rate-limit';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
 	app.use(CustomMorgan());
+
+	initSecurity(app);
 
 	initSwagger(app);
 
@@ -13,6 +22,49 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+function initSecurity(app) {
+	//cors
+	app.enableCors();
+
+	app.set('trust proxy', 1);
+
+	// helmet
+	app.use(helmet());
+	// todo: set additional helmet setting
+	// app.use(helmet.contentSecurityPolicy({
+	//     directives: {
+	//         defaultSrc: ["'self'"],
+	//         styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com']
+	//     }
+	// }))
+	// app.use(helmet.permittedCrossDomainPolicies())
+	// app.use(helmet.referrerPolicy({policy: 'same-origin'}))
+
+	app.use(
+		expectCt({
+			enforce: true,
+			maxAge: 123,
+		}),
+	);
+
+	// todo 이거 옵션 조정하기
+	// app.use(csurf({cookie: true}));
+
+	// todo 이거 옵션 조정하기
+	app.use(
+		rateLimit({
+			windowMs: 15 * 60 * 1000, // 15 minutes
+			max: 100, // limit each IP to 100 requests per windowMs
+		}),
+	);
+
+	// todo: add html sanitize
+
+	// todo: add xss protector
+
+	// todo: add sql injection protector
+}
 
 function initSwagger(app) {
 	const options = new DocumentBuilder()
