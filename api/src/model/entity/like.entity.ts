47fe9e8259c820c099272c9ea02f127172c885c7
demetoir <wnsqlehlswk@naver.com>
2020-07-11 20:21:56 +0900

    fix: delete unused column in like entity

--- api/src/model/entity/like.entity.ts
+++ api/src/model/entity/like.entity.ts
@@ -1,15 +1,9 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { AbstractBaseEntity } from './abstractBase.entity';
 
 @Entity({ name: 'likes' })
 export class LikeEntity extends AbstractBaseEntity {
-	@Column({ name: 'ref_type', type: 'bigint', nullable: true })
-	refType: bigint;
-
-	@Column({ name: 'ref_id', type: 'bigint', nullable: true })
-	refId: bigint;
-
 	@ManyToOne(() => UserEntity, (user) => user.likes)
 	@JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
 	user: UserEntity;
