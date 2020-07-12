b1e995677b148c264a5575d2ab4cd34952940f6e
demetoir <wnsqlehlswk@naver.com>
2020-07-12 23:03:15 +0900

    chore: add method blueprint of image controller

--- api/src/component/image/image.controller.ts
+++ api/src/component/image/image.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { logger } from '../../libs/winstonToolkit';
 
-@Controller('image')
-export class ImageController {}
+@Controller('images')
+export class ImageController {
+	private logger: any;
+
+	constructor() {
+		this.logger = logger;
+	}
+
+	//type by user profile, expression thumbnail, team profile image
+
+	@Get('/')
+	async getMany() {
+		return 'get many images';
+	}
+
+	@Post('/')
+	async createOne() {
+		return 'create images';
+	}
+
+	@Get('/:id')
+	async getOne() {
+		return 'get images';
+	}
+
+	@Put('/:id')
+	async updateOne() {
+		return 'update images';
+	}
+
+	@Patch('/:id')
+	async updateOnePartial() {
+		return 'update images partial';
+	}
+
+	@Delete('/:id')
+	async deleteOne() {
+		return 'delete images';
+	}
+}
