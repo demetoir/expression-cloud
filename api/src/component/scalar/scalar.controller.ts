0576c1154f67a8cf02a887468f957f74aa7a0149
demetoir <wnsqlehlswk@naver.com>
2020-07-12 23:04:12 +0900

    chore: add method blueprint of scalar controller

--- api/src/component/scalar/scalar.controller.ts
+++ api/src/component/scalar/scalar.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { logger } from '../../libs/winstonToolkit';
 
-@Controller('scalar')
-export class ScalarController {}
+@Controller('scalars')
+export class ScalarController {
+	private logger: any;
+
+	constructor() {
+		this.logger = logger;
+	}
+
+	@Get('/')
+	async getMany() {
+		return;
+	}
+
+	@Post('/')
+	async createOne() {
+		return;
+	}
+
+	@Get('/:id')
+	async getOne() {
+		return;
+	}
+
+	@Put('/:id')
+	async updateOne() {
+		return;
+	}
+
+	@Patch('/:id')
+	async updatePartialOne() {
+		return;
+	}
+
+	@Delete('/:id')
+	async deleteOne() {
+		return;
+	}
+}
