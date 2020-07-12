19fd325c55b953838fa47ae04dbad93354d17600
demetoir <wnsqlehlswk@naver.com>
2020-07-12 23:01:34 +0900

    chore: add method blueprint of collection controller

--- api/src/component/collection/collection.controller.ts
+++ api/src/component/collection/collection.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { logger } from '../../libs/winstonToolkit';
 
-@Controller('collection')
-export class CollectionController {}
+@Controller('collections')
+export class CollectionController {
+	private logger: any;
+
+	constructor() {
+		this.logger = logger;
+	}
+
+	@Get('/')
+	async getMany() {
+		return 'get many collection';
+	}
+
+	@Post('/')
+	async createOne() {
+		return 'create collection';
+	}
+
+	@Get('/:id')
+	async getOne() {
+		return 'get collection';
+	}
+
+	@Put('/:id')
+	async updateOne() {
+		return 'update collection';
+	}
+
+	@Patch('/:id')
+	async updateOnePartial() {
+		return 'update collection partial';
+	}
+
+	@Delete('/:id')
+	async deleteOne() {
+		return 'delete collection partial';
+	}
+}
