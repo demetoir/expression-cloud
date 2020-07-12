c53d7a56a8e2b5352b358fb45c6225f4f72a91d1
demetoir <wnsqlehlswk@naver.com>
2020-07-12 22:25:11 +0900

    chore: register all module to app module

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -5,9 +5,31 @@ import { AppService } from './app.service';
 import { config as ormConfig } from '../model/configLoader';
 import { UserModule } from '../component/user/user.module';
 import { AuthModule } from '../component/auth/auth.module';
+import { CollectionModule } from '../component/collection/collection.module';
+import { ExpressionModule } from '../component/expression/expression.module';
+import { HistoryModule } from '../component/history/history.module';
+import { NoticeModule } from '../component/notice/notice.module';
+import { ScalarModule } from '../component/scalar/scalar.module';
+import { TagModule } from '../component/tag/tag.module';
+import { TeamModule } from '../component/team/team.module';
+import { VectorModule } from '../component/vector/vector.module';
+import { ImageModule } from '../component/image/image.module';
 
 @Module({
-	imports: [TypeOrmModule.forRoot(ormConfig), UserModule, AuthModule],
+	imports: [
+		TypeOrmModule.forRoot(ormConfig),
+		UserModule,
+		AuthModule,
+		CollectionModule,
+		ExpressionModule,
+		HistoryModule,
+		NoticeModule,
+		ScalarModule,
+		TagModule,
+		TeamModule,
+		VectorModule,
+		ImageModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
