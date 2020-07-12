e737fe73aee00c6d558f227674534f003b640831
demetoir <wnsqlehlswk@naver.com>
2020-07-12 23:04:31 +0900

    chore: add method blueprint of tag controller

--- api/src/component/tag/tag.controller.ts
+++ api/src/component/tag/tag.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { logger } from '../../libs/winstonToolkit';
 
-@Controller('tag')
-export class TagController {}
+@Controller('tags')
+export class TagController {
+	private logger: any;
+
+	constructor() {
+		this.logger = logger;
+	}
+
+	@Get('/')
+	async getMany() {
+		return;
+	}
+
+	@Post('/')
+	async createOne() {
+		return;
+	}
+
+	@Get('/:id')
+	async getOne() {
+		return;
+	}
+
+	@Put('/:id')
+	async updateOne() {
+		return;
+	}
+
+	@Patch('/')
+	async updateOnePartial() {
+		return;
+	}
+
+	@Delete('/')
+	async deleteOne() {
+		return;
+	}
+}
