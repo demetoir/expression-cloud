0f3018d9146026837cdbcb2c9cdcece34cd88518
demetoir <wnsqlehlswk@naver.com>
2020-07-12 23:03:35 +0900

    chore: add method blueprint of notice controller

--- api/src/component/notice/notice.controller.ts
+++ api/src/component/notice/notice.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { logger } from '../../libs/winstonToolkit';
 
-@Controller('notice')
-export class NoticeController {}
+@Controller('notices')
+export class NoticeController {
+	private logger: any;
+
+	constructor() {
+		this.logger = logger;
+	}
+
+	@Get('/')
+	async getMany() {
+		return '';
+	}
+
+	@Post('/')
+	async create() {
+		return '';
+	}
+
+	@Get('/:id')
+	async getOne() {
+		return '';
+	}
+
+	@Put('/:id')
+	async updateOne() {
+		return '';
+	}
+
+	@Patch('/:id')
+	async updateOnePartial() {
+		return '';
+	}
+
+	@Delete('/:id')
+	async deleteOne() {
+		return '';
+	}
+
+	@Post('/:id/actions/read')
+	async read() {
+		return '';
+	}
+
+	@Post('/:id/actions/undo-read')
+	async undoRead() {
+		return '';
+	}
+}
