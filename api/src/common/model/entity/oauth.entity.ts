0b979fb5d583fcc1528f6126fc43d273c2334aab
demetoir <wnsqlehlswk@naver.com>
2020-07-26 19:06:46 +0900

    chore: add todo

--- api/src/common/model/entity/oauth.entity.ts
+++ api/src/common/model/entity/oauth.entity.ts
@@ -7,8 +7,18 @@ export class OauthEntity extends AbstractBaseEntity {
 	@Column({ name: 'type', type: 'bigint', nullable: false })
 	type: number;
 
-	@Column({ name: 'authId', type: 'text', nullable: false })
+	@Column({ name: 'auth_id', type: 'text', nullable: false })
 	authId: string;
+
+	// todo: oauth2.0인즈시 필요한 컬럼 종류 정리하기
+	//  google, github, naver, kakao, facebook 이거 5개 호환용
+	//  client_id: string;
+	//  client_secret: string;
+	//  code: string;
+	//  redirect_uri: string;
+	//  state: string;
+	//  grant_type: string;
+	//  provider: string;
 
 	@OneToOne(() => UserEntity, (user) => user.oauth)
 	@JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
