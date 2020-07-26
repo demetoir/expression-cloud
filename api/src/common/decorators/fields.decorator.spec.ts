bd5e0daab08a7c85c28b510b6d2429e0af3cf22d
demetoir <wnsqlehlswk@naver.com>
2020-07-26 22:59:09 +0900

    feature: add fields param decorator

---
+++ api/src/common/decorators/fields.decorator.spec.ts
@@ -0,0 +1,24 @@
+import { _decorator, Fields } from './fields.decorator';
+
+describe('ResourceId decorator', () => {
+	it('should be declare', function () {
+		expect(Fields).toBeDefined();
+		expect(_decorator).toBeDefined();
+	});
+
+	// todo: 데코레이터 테스트시 interface 를 mock할 방법을 찾아야함
+	it('should be send args (token, ctx)', function () {
+		expect(false);
+		// _decorator(token, executionContext);
+	});
+
+	it('should be parse resource id as number', function () {
+		expect(false);
+		// _decorator(token, executionContext);
+	});
+
+	it('should be throw error, if resource id is not number', function () {
+		expect(false);
+		// _decorator(token, executionContext);
+	});
+});
