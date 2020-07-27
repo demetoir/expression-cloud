85c30c5caf4c56dae4d3c6a6c8f40552b1486fdd
demetoir <wnsqlehlswk@naver.com>
2020-07-27 21:14:15 +0900

    enhance: add test of includes decorator

--- api/src/common/decorators/includes.decorator.spec.ts
+++ api/src/common/decorators/includes.decorator.spec.ts
@@ -1,24 +1,84 @@
-import { _decorator, Includes } from './includes.decorator';
+import { ExecutionContext } from '@nestjs/common';
+import { HttpArgumentsHost } from '@nestjs/common/interfaces';
+import { mock } from 'jest-mock-extended';
+import { _Includes, Includes } from './includes.decorator';
 
-describe('ResourceId decorator', () => {
+function getExecutionContextMock(request): ExecutionContext {
+	const httpArgumentHost = mock<HttpArgumentsHost>();
+	// noinspection TypeScriptUnresolvedFunction
+	httpArgumentHost.getRequest.mockReturnValue(request);
+
+	const executionContext = mock<ExecutionContext>();
+
+	executionContext.switchToHttp.mockReturnValue(httpArgumentHost);
+
+	return executionContext;
+}
+
+describe('includes param decorator', () => {
 	it('should be declare', function () {
 		expect(Includes).toBeDefined();
-		expect(_decorator).toBeDefined();
+		expect(_Includes).toBeDefined();
 	});
 
-	// todo: 데코레이터 테스트시 interface 를 mock할 방법을 찾아야함
-	it('should be send args (token, ctx)', function () {
-		expect(false);
-		// _decorator(token, executionContext);
+	it('should be send args (data, ctx)', function () {
+		const request = {
+			query: {
+				includes: '12,1231,23',
+			},
+		};
+
+		const context = getExecutionContextMock(request);
+		const result = _Includes(null, context);
+
+		expect(result).toBeInstanceOf(Array);
 	});
 
-	it('should be parse resource id as number', function () {
-		expect(false);
-		// _decorator(token, executionContext);
+	it('should be return array type', function () {
+		const request = {
+			query: {
+				includes: '12,1231,23',
+			},
+		};
+
+		const context = getExecutionContextMock(request);
+		const result = _Includes(null, context);
+
+		expect(result).toBeInstanceOf(Array);
 	});
 
-	it('should be throw error, if resource id is not number', function () {
-		expect(false);
-		// _decorator(token, executionContext);
+	it('should be parse include', function () {
+		const request = {
+			query: {
+				includes: 'a,b,c',
+			},
+		};
+
+		const context = getExecutionContextMock(request);
+		const result = _Includes(null, context);
+
+		expect(result).toStrictEqual(['a', 'b', 'c']);
+	});
+
+	it('should be empty array if includes not in query', function () {
+		const request = {
+			query: {},
+		};
+
+		const context = getExecutionContextMock(request);
+		const result = _Includes(null, context);
+
+		expect(result).toStrictEqual([]);
+	});
+
+	it('should be ignore empty string', function () {
+		const request = {
+			query: { includes: '11,,,,5&&,' },
+		};
+
+		const context = getExecutionContextMock(request);
+		const result = _Includes(null, context);
+
+		expect(result).toStrictEqual(['11', '5&&']);
 	});
 });
