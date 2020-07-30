2343a63fe1027b5a19520806eb5dcb48bbfa78da
demetoir <wnsqlehlswk@naver.com>
2020-07-30 23:03:34 +0900

    chore: add todo comment

--- api/src/common/model/entity/user.entity.ts
+++ api/src/common/model/entity/user.entity.ts
@@ -19,6 +19,15 @@ import { UserProfileImageEntity } from './userProfileImage.entity';
 import { ApiProperty } from '@nestjs/swagger';
 
 // todo 상수로 테이블 이름 분리하기
+// todo: 엔티티 클래스에 너무 많은 엔티가 붙는다
+//   이거를 3단계로 상속 받아서 처리하면 어떨지 생각해야한다
+//   예를들어 기본적인 typeorm entity decorator 있는놈을 상속받은 놈에서
+//   class-validation 추가하고 또 상속한뒤 swagger decorator를 추가한다
+//   그리고 이놈들은 모두 폴더 한단계 파서 몰아넣고 최송 자식만을 익스포트 해버리면 되지 않을까?
+//   적어도 이렇게 하면 관심사가  데코레이터별로 나누어진다는 장점이 있다.
+//   그렇지만 상속 관계를 이용해서 구현했기 때문에 종속성이 생기고, 프로퍼티 세이프 하지 않음...
+//   safe하게 하려면 각 클래스마다 제대로 상속 받았는지 property 검사하도록 테스트 작성하면 된다
+//
 @Entity({ name: 'users' })
 export class UserEntity extends AbstractBaseEntity {
 	@ApiProperty({
--- api/src/user/userCRUD/userCRUD.controller.ts
+++ api/src/user/userCRUD/userCRUD.controller.ts
@@ -4,6 +4,9 @@ import { Crud, CrudController } from '@nestjsx/crud';
 import { UserEntity } from '../../common/model/entity/user.entity';
 import { ApiProperty, ApiTags } from '@nestjs/swagger';
 
+// todo 각 CRUD마다 DTO class 선언하기
+// todo 도메인 별 DTO class를 따로 폴더 파서 넣기
+
 class UserCreateDto {
 	@ApiProperty({
 		required: true,
@@ -36,7 +39,8 @@ class UserCreateDto {
 // })
 @ApiTags('User')
 // nest.js/crud 상에서 dto를 안사용하면 자동으로 entity를 request body 그대로 사용하니
-// create, update, replace(partial_update)dto 를 따로 주입해야한다
+// create, update, replace(partial_update)dto 를 따로 주입해야한다기
+// todo: 여기 @CRUD 데코레이터의 option 설정 공부하고 예제좀 만들기
 @Crud({
 	model: {
 		type: UserEntity,
@@ -48,4 +52,47 @@ class UserCreateDto {
 @Controller('/v1/users')
 export class UserCRUDController implements CrudController<UserEntity> {
 	constructor(public service: UserCRUDService) {}
+
+	// todo: 기본놈에대가 오버라이드 하는 예제 만들기
+	// get base(): CrudController<UserEntity> {
+	// 	return this;
+	// }
+	//
+	// @Override()
+	// getMany(@ParsedRequest() req: CrudRequest) {
+	// 	return this.base.getManyBase(req);
+	// }
+	//
+	// @Override('getOneBase')
+	// getOneAndDoStuff(@ParsedRequest() req: CrudRequest) {
+	// 	return this.base.getOneBase(req);
+	// }
+	//
+	// @Override()
+	// createOne(@ParsedRequest() req: CrudRequest, @ParsedBody() dto: Hero) {
+	// 	return this.base.createOneBase(req, dto);
+	// }
+	//
+	// @Override()
+	// createMany(
+	// 	@ParsedRequest() req: CrudRequest,
+	// 	@ParsedBody() dto: CreateManyDto<Hero>,
+	// ) {
+	// 	return this.base.createManyBase(req, dto);
+	// }
+	//
+	// @Override('updateOneBase')
+	// coolFunction(@ParsedRequest() req: CrudRequest, @ParsedBody() dto: Hero) {
+	// 	return this.base.updateOneBase(req, dto);
+	// }
+	//
+	// @Override('replaceOneBase')
+	// awesomePUT(@ParsedRequest() req: CrudRequest, @ParsedBody() dto: Hero) {
+	// 	return this.base.replaceOneBase(req, dto);
+	// }
+	//
+	// @Override()
+	// async deleteOne(@ParsedRequest() req: CrudRequest) {
+	// 	return this.base.deleteOneBase(req);
+	// }
 }
