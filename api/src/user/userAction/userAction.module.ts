ccac2e88d970b67962d0681fca7bb40a4fc06154
demetoir <wnsqlehlswk@naver.com>
2020-08-01 23:37:59 +0900

    fix: fix DI userAction.module.ts

--- api/src/user/userAction/userAction.module.ts
+++ api/src/user/userAction/userAction.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserActionController } from './userAction.controller';
 import { UserActionService } from './userAction.service';
+import { UserRepository } from '../userRepository/user.repository';
 
 @Module({
 	controllers: [UserActionController],
-	providers: [UserActionService],
+	providers: [UserActionService, UserRepository],
 })
 export class UserActionModule {}
