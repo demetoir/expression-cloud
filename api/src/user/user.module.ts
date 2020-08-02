93fbf4d19a83ccf4ca4bdc88ec9049a173b45cce
demetoir <wnsqlehlswk@naver.com>
2020-08-02 22:49:35 +0900

    enhance: add userRepository module to userModule

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserActionModule } from './userAction/userAction.module';
 import { UserSettingModule } from './userSetting/userSetting.module';
 import { UserCRUDModule } from './userCRUD/userCRUD.module';
+import { UserRepositoryModule } from './userRepository/userRepository.module';
 
 @Module({
-	imports: [UserActionModule, UserCRUDModule, UserSettingModule],
+	imports: [
+		UserActionModule,
+		UserCRUDModule,
+		UserSettingModule,
+		UserRepositoryModule,
+	],
 })
 export class UserModule {}
