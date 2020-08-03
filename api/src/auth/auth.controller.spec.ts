d9a9fea822af5210e22292afd372eb9adaa16ffc
demetoir <wnsqlehlswk@naver.com>
2020-08-03 23:29:12 +0900

    fix: broken test, DI was broken

--- api/src/auth/auth.controller.spec.ts
+++ api/src/auth/auth.controller.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local/local.strategy';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './jwt/jwtEnv';
 
 describe('Auth Controller', () => {
 	let controller: AuthController;
@@ -7,6 +13,14 @@ describe('Auth Controller', () => {
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [AuthController],
+			providers: [AuthService, LocalStrategy, JwtStrategy],
+			imports: [
+				PassportModule,
+				JwtModule.register({
+					secret: jwtConstants.secret,
+					signOptions: { expiresIn: '60h' },
+				}),
+			],
 		}).compile();
 
 		controller = module.get<AuthController>(AuthController);
--- api/src/auth/auth.service.spec.ts
+++ api/src/auth/auth.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './jwt/jwtEnv';
+import { LocalStrategy } from './local/local.strategy';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 describe('AuthService', () => {
 	let service: AuthService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [AuthService],
+			providers: [AuthService, LocalStrategy, JwtStrategy],
+			imports: [
+				PassportModule,
+				JwtModule.register({
+					secret: jwtConstants.secret,
+					signOptions: { expiresIn: '60h' },
+				}),
+			],
 		}).compile();
 
 		service = module.get<AuthService>(AuthService);
