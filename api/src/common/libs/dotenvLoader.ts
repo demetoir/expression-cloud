ef4d8bacb4e389c2cb0d95829809fa07a137afdd
demetoir <wnsqlehlswk@naver.com>
2020-08-05 00:43:59 +0900

    enhance: add dotenvLoader.ts

---
+++ api/src/common/libs/dotenvLoader.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+
+export function loadDotEnv({ devPath, prodPath }): void {
+	const NODE_ENV = process.env.NODE_ENV || 'development';
+
+	console.log(`load dot env as ${NODE_ENV} mode`);
+
+	let path;
+	if (NODE_ENV === 'development') {
+		path = devPath;
+	} else if (NODE_ENV === 'production') {
+		path = prodPath;
+	} else {
+		throw new Error(`NODE_ENV ${NODE_ENV} is not expected value`);
+	}
+
+	path = resolve(path);
+	require('dotenv').config({ path });
+
+	console.log(process.env.MYSQL_PORT);
+}
