ddd1a88c83d6b4e572a40ad4a289ed2b01b597cf
demetoir <wnsqlehlswk@naver.com>
2020-08-10 00:38:25 +0900

    fix: not show as array at response of getMany in CRUD api response

--- api/src/common/libs/nestjsCRUDswaggerToolkit/responseDecorator/apiOkReponse.decorator.ts
+++ api/src/common/libs/nestjsCRUDswaggerToolkit/responseDecorator/apiOkReponse.decorator.ts
@@ -7,6 +7,7 @@ import { ApiResponse } from '@nestjs/swagger';
 export const ApiOkResponse = (option: ApiCRUDOption): ApiCRUDDecorator =>
 	ApiResponse({
 		type: option.type,
+		isArray: option.isArray,
 		status: 200,
 		description: option.description || 'request success',
 	});
