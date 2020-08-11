10991657793bd80ade32b4a657422559fff34d1b
demetoir <wnsqlehlswk@naver.com>
2020-08-11 21:03:43 +0900

    docs: add docs for ApiAddTag.ts

--- api/src/common/libs/nestjsCRUDToolkit/openApi/ApiAddTag.ts
+++ api/src/common/libs/nestjsCRUDToolkit/openApi/ApiAddTag.ts
@@ -17,6 +17,17 @@ const _ApiAddTag = ({ name, description }: ApiAddTagOptions) => {
 	};
 };
 
+/**
+ * controller class decorate 하면 바로다 tag name, description, controller 지정한
+ *
+ * @param name
+ * @param description
+ * @constructor
+ */
 export const ApiAddTag = ({ name, description }: ApiAddTagOptions) => {
+	// _ApiAddTag tag 이름과 설명을 추가만 한다.
+	// ApiTag는 컨트롤러에 tag 지정 및 이름 설정 만한다.
+	// 둘다 해주어야 데코레이터가 적용될 컨트롤러와 tag name, description 모두 적용된다
+
 	return applyDecorators(_ApiAddTag({ name, description }), ApiTags(name));
 };
