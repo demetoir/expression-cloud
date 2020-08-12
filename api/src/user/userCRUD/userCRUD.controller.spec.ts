be6edab41909921d57f0dc052367d64b58a5f092
demetoir <wnsqlehlswk@naver.com>
2020-08-12 23:27:12 +0900

    fix: broken test

--- api/src/user/userCRUD/userCRUD.controller.spec.ts
+++ api/src/user/userCRUD/userCRUD.controller.spec.ts
@@ -1,30 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserCRUDController } from './userCRUD.controller';
 import { UserCRUDService } from './userCRUD.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { MockRepository } from '../../../test/lib/mock/MockRepository';
-import { UserEntity } from '../../common/model/entity/user.entity';
+import { MockCrudService } from '../../../test/lib/mock/CrudService.mock';
 
 describe('UserCRUD Controller', () => {
 	let controller: UserCRUDController;
+	let service: MockCrudService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [UserCRUDController],
 			providers: [
-				UserCRUDService,
 				{
-					provide: getRepositoryToken(UserEntity),
-					useClass: MockRepository,
+					provide: UserCRUDService,
+					useClass: MockCrudService,
 				},
 			],
 		}).compile();
 
 		controller = module.get(UserCRUDController);
+		service = module.get(UserCRUDService);
 	});
 
 	it('should be create by DI', function () {
 		expect(controller).toBeDefined();
+		expect(service).toBeDefined();
 	});
 
 	it('should be define CRUD method', () => {
