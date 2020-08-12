e7bab9f17f6dd3465ef82a2061f465e835c3aeec
demetoir <wnsqlehlswk@naver.com>
2020-08-12 23:23:42 +0900

    enhance: add CrudService.mock.ts

---
+++ api/test/lib/mock/CrudService.mock.ts
@@ -0,0 +1,9 @@
+export class MockCrudService {
+	public getMany = jest.fn();
+	public getOne = jest.fn();
+	public createOne = jest.fn();
+	public createMany = jest.fn();
+	public updateOne = jest.fn();
+	public replaceOne = jest.fn();
+	public deleteOne = jest.fn();
+}
