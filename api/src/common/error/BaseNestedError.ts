0a177324dd9a3d071fc11ef356dd591fd7cd8355
demetoir <wnsqlehlswk@naver.com>
2020-08-15 22:30:07 +0900

    enhance: make able to 0 arg construct on BaseNestedError.ts

--- api/src/common/error/BaseNestedError.ts
+++ api/src/common/error/BaseNestedError.ts
@@ -1,8 +1,9 @@
 export class BaseNestedError extends Error {
 	private readonly inner: Error | void;
 
-	constructor(message: string, e: Error | void) {
-		super(message);
+	constructor(message: string | void, e: Error | void) {
+		super();
+		this.message = message || '';
 		this.inner = e;
 	}
 }
