f54c85cf58fcab0e90dac4a79bc69c79ed0a5950
demetoir <wnsqlehlswk@naver.com>
2020-08-15 23:56:39 +0900

    add: more error for jwt auth service

--- api/src/auth/JWTAuth/error/index.ts
+++ api/src/auth/JWTAuth/error/index.ts
@@ -5,3 +5,7 @@ export class JWTPayloadTypeError extends BaseNestedError {}
 export class JWTMalformedError extends BaseNestedError {}
 
 export class JWTInvalidSignatureError extends BaseNestedError {}
+
+export class JWTExpiredError extends BaseNestedError {}
+
+export class InvalidJwtPayloadError extends BaseNestedError {}
