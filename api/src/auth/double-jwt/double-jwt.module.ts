f349892aca236d337561fe4050c6e972e7d42ead
demetoir <wnsqlehlswk@naver.com>
2020-08-17 21:37:25 +0900

    fix: import path

--- api/src/auth/double-jwt/double-jwt.module.ts
+++ api/src/auth/double-jwt/double-jwt.module.ts
@@ -1,8 +1,8 @@
 import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { DoubleJwtService } from './double-jwt.service';
-import { DoubleJwtOptionService } from './jwt-option.service';
 import { JwtPassportModule } from './strategy/jwtPassport.module';
+import { DoubleJwtOptionService } from './double-jwt-option.service';
 
 @Module({
 	imports: [
