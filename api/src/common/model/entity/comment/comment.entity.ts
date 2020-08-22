026b64dace70ecc10375716c3b5e55d8d73f2043
demetoir <wnsqlehlswk@naver.com>
2020-08-22 21:55:15 +0900

    refactoring(api): apply comment interface to entity

--- api/src/common/model/entity/comment/comment.entity.ts
+++ api/src/common/model/entity/comment/comment.entity.ts
@@ -1,9 +1,11 @@
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { UserEntity } from '../user/user.entity';
 import { BaseEntity } from '../base/base.entity';
+import { IComment } from './comment.interface';
+import { IUserEntity } from '../user/user.interface';
 
 @Entity({ name: 'comments' })
-export class CommentEntity extends BaseEntity {
+export class CommentEntity extends BaseEntity implements IComment {
 	@Column({ name: 'content', type: 'text', nullable: false })
 	content: string;
 
@@ -15,5 +17,5 @@ export class CommentEntity extends BaseEntity {
 
 	@ManyToOne(() => UserEntity, (user) => user.comments)
 	@JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
-	user: UserEntity;
+	user: IUserEntity;
 }
