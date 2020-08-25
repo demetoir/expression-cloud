84625f12b216a6bac60e960ae1a719c604c753f6
demetoir <wnsqlehlswk@naver.com>
2020-08-25 21:39:05 +0900

    enhance(api): helper function for jest

--- api/test/lib/helper/jestHelper.ts
+++ api/test/lib/helper/jestHelper.ts
@@ -1,3 +1,7 @@
 export function expectShouldNotCallThis() {
 	expect('should not call this').toEqual('but called');
 }
+
+export function expectShouldBeImplementTest() {
+	expect('this test case must implement').toEqual('but not');
+}
