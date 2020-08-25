daf5faf9a64f9a7015bd4d221b989c891d3be097
demetoir <wnsqlehlswk@naver.com>
2020-08-25 20:29:59 +0900

    feature(api): apply nest-crud service at user-like.service.ts

--- api/src/user/user-like/user-like.service.spec.ts
+++ api/src/user/user-like/user-like.service.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserLikeService } from './user-like.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockRepository } from '../../../test/lib/mock/MockRepository';
+import { UserLikeEntity } from './user-like.entity';
+import { Repository } from 'typeorm/index';
 
 describe('UserLikeService', () => {
 	let service: UserLikeService;
+	let repository: Repository<UserLikeEntity>;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [UserLikeService],
+			providers: [
+				UserLikeService,
+				{
+					provide: getRepositoryToken(UserLikeEntity),
+					useClass: MockRepository,
+				},
+			],
 		}).compile();
 
-		service = module.get<UserLikeService>(UserLikeService);
+		service = module.get(UserLikeService);
+		repository = module.get(getRepositoryToken(UserLikeEntity));
 	});
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+		expect(repository).toBeDefined();
+	});
+
+	it('should define method', () => {
+		expect(service.createOne).toBeDefined();
+		expect(service.getMany).toBeDefined();
+		expect(service.deleteOne).toBeDefined();
 	});
 });
--- api/src/user/user-like/user-like.service.ts
+++ api/src/user/user-like/user-like.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm/index';
+import { UserLikeEntity } from './user-like.entity';
 
 @Injectable()
-export class UserLikeService {}
+export class UserLikeService extends TypeOrmCrudService<UserLikeEntity> {
+	constructor(
+		@InjectRepository(UserLikeEntity) repo: Repository<UserLikeEntity>,
+	) {
+		super(repo);
+	}
+}
