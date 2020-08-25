6a00aa266760b47eef6b0ffd2be8f0455c709371
demetoir <wnsqlehlswk@naver.com>
2020-08-25 23:37:59 +0900

    init(api): add UserFactory
    close #53

---
+++ api/test/user/user/user.factory.spec.ts
@@ -0,0 +1,38 @@
+import { createConnection, getConnection, Repository } from 'typeorm';
+import { UserEntity } from '../../../src/user/user/user.entity';
+import { ormConfig } from '../../../src/common/model/configLoader';
+import { UserFactory } from './user.factory';
+
+describe('user factory', () => {
+	let userRepository: Repository<UserEntity>;
+	let connection;
+
+	beforeAll(async () => {
+		connection = await createConnection(ormConfig);
+		await connection.synchronize();
+
+		userRepository = connection.getRepository(UserEntity);
+	});
+
+	beforeEach(async () => {
+		await getConnection().synchronize(true);
+	});
+
+	afterAll(async () => {
+		connection.close();
+	});
+
+	it('should able to get repository from connection manager', function () {
+		expect(userRepository).toBeDefined();
+	});
+
+	it('should build random user', async function () {
+		const user = UserFactory.build();
+
+		await connection.manager.save(user);
+
+		const stored = await userRepository.findOne({ id: user.id });
+
+		expect(stored).toEqual(user);
+	});
+});
---
+++ api/test/user/user/user.factory.ts
@@ -0,0 +1,29 @@
+import { UserEntity } from '../../../src/user/user/user.entity';
+import { IUser } from '../../../src/user/user/user.interface';
+import { FAKER_SEED } from '../../constant';
+import { internet, lorem, name, seed } from 'faker';
+
+seed(FAKER_SEED);
+
+export class UserFactory {
+	name: string;
+
+	email: string;
+
+	description: string;
+
+	isAnonymous: boolean;
+
+	likedCount: number;
+
+	forkedCount: number;
+
+	static build(): IUser {
+		const user = new UserEntity();
+		user.name = name.findName();
+		user.email = internet.email();
+		user.description = lorem.sentence();
+
+		return user;
+	}
+}
