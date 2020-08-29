cb90b8ba2b5d637b2b0f75a38f50ad70844814da
demetoir <wnsqlehlswk@naver.com>
2020-08-29 12:21:42 +0900

    enhance(api): add jest config option and add global setup script

---
+++ api/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	setupFilesAfterEnv: ['./test/jest.setup.js'],
+
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
---
+++ api/test/jest.setup.js
@@ -0,0 +1,11 @@
+const { createConnection } = require('typeorm');
+const { ormConfig } = require('../src/common/model/configLoader');
+
+module.exports = async () => {
+	const connection = await createConnection({
+		...ormConfig,
+		dropSchema: true,
+	});
+
+	await connection.synchronize();
+};
