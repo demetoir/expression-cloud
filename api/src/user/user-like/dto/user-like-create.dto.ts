7c8eee1661c338f1de22c1f948381c43a4ff47a6
demetoir <wnsqlehlswk@naver.com>
2020-08-29 20:57:27 +0900

    feature(api): add user-like-create.dto.ts

--- api/src/user/user-like/dto/user-like-create.dto.ts
+++ api/src/user/user-like/dto/user-like-create.dto.ts
@@ -1,9 +1,22 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsPositive } from 'class-validator';
+import { AbstractBaseDto } from '../../../common/abstractBaseDto';
+import { UserLikeEntity } from '../user-like.entity';
+import { plainToClass } from 'class-transformer';
 
-export class UserLikeCreateDto {
+export class UserLikeCreateDto extends AbstractBaseDto {
 	@IsNumber()
+	@IsPositive()
 	fromUserId: number;
 
 	@IsNumber()
+	@IsPositive()
 	toUserId: number;
+
+	public static fromBody(body: unknown): AbstractBaseDto {
+		return plainToClass(UserLikeCreateDto, body);
+	}
+
+	public toEntity(): UserLikeEntity {
+		return plainToClass(UserLikeEntity, this);
+	}
 }
