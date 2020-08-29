7a72314ea08cb841b0cd7130e0497a2d3e536ffc
demetoir <wnsqlehlswk@naver.com>
2020-08-29 13:19:24 +0900

    enhance(api): add jest global setup script for wait db connection

--- api/test/jest.setup.js
+++ api/test/jest.setup.js
@@ -1,11 +1,37 @@
 const { createConnection } = require('typeorm');
 const { ormConfig } = require('../src/common/model/configLoader');
+const _ = require('lodash');
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 module.exports = async () => {
-	const connection = await createConnection({
-		...ormConfig,
-		dropSchema: true,
-	});
+	let connection;
+	const MAX_RETRY = 5;
+
+	for await (let i of _.range(0, MAX_RETRY)) {
+		try {
+			connection = await createConnection({
+				...ormConfig,
+				dropSchema: true,
+			});
+
+			break;
+		} catch (e) {
+			console.log(`can not connect to db retry ${i}`);
+		}
+
+		console.log(`sleep wait for db ${2000}ms`);
+		await sleep(2000);
+	}
+
+	if (!connection) {
+		throw new Error(
+			'can not connection to db, even if retry for MAX_RETRY',
+		);
+	}
+
 	await connection.synchronize();
 
 	global.connection = connection;
