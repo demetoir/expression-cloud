0c4ea6dbd485c681482c44e234bd1de5733ad04d
demetoir <wnsqlehlswk@naver.com>
2020-08-31 23:15:21 +0900

    test(api/user-like): add more test to controller

--- api/test/user/user-like/user-like.controller.spec.ts
+++ api/test/user/user-like/user-like.controller.spec.ts
@@ -2,6 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserLikeController } from '../../../src/user/user-like/user-like.controller';
 import { UserLikeService } from '../../../src/user/user-like/user-like.service';
 import { MockUserLikeService } from './user-like.service.mock';
+import { expectShouldNotCallThis } from '../../lib/helper/jestHelper';
+import { CRUDRequestMockBuilder } from '../../lib/mock/CRUDRequest.MockBuilder';
+import { UserLikeEntity } from '../../../src/user/user-like/user-like.entity';
+import { QueryFailedError } from 'typeorm/index';
+import { MysqlErrorCodes } from 'mysql-error-codes';
+import { DatabaseConstraintFailError } from '../../../src/common/error/database-constraint-fail.error';
+import { AnyOtherError } from '../../lib/error/any-other.error';
 
 describe('UserLike Controller', () => {
 	let controller: UserLikeController;
@@ -22,5 +29,120 @@ describe('UserLike Controller', () => {
 	it('should be defined', () => {
 		expect(controller).toBeDefined();
 		expect(service).toBeDefined();
+	});
+
+	describe('method getMany', () => {
+		it('should call this.base.getMany', async function () {
+			// given
+			const req = CRUDRequestMockBuilder.build();
+			const expectedResult = {};
+			service.getMany.mockImplementation(() => {
+				return expectedResult;
+			});
+
+			// when
+			const result = await controller.getMany(req);
+
+			// than
+			expect(result).toEqual(expectedResult);
+			expect(service.getMany.mock.calls.length).toBe(1);
+			expect(service.getMany.mock.calls[0]).toEqual([req]);
+		});
+	});
+
+	describe('method createOne', () => {
+		it('should call this.base.createOne', async function () {
+			// given
+			const req = CRUDRequestMockBuilder.build();
+			const entity = new UserLikeEntity();
+
+			const stored = new UserLikeEntity();
+
+			service.createOne.mockImplementation(() => {
+				return stored;
+			});
+
+			//when
+			const result = await controller.createOne(req, entity);
+
+			//than
+			expect(result).toEqual(stored);
+			expect(service.createOne.mock.calls.length).toBe(1);
+			expect(service.createOne.mock.calls[0]).toEqual([req, entity]);
+		});
+
+		it('raise DatabaseConstraintFailError, if service raise QueryFailedError because of ER_NO_REFERENCED_ROW_2', async function () {
+			// given
+			const req = CRUDRequestMockBuilder.build();
+			const entity = new UserLikeEntity();
+
+			service.createOne = jest.fn().mockImplementation(() => {
+				const error = new QueryFailedError(
+					'dummy query',
+					[],
+					new AnyOtherError(),
+				);
+
+				error['errno'] = MysqlErrorCodes.ER_NO_REFERENCED_ROW_2;
+
+				throw error;
+			});
+
+			//when
+			try {
+				await controller.createOne(req, entity);
+
+				expectShouldNotCallThis();
+			} catch (e) {
+				//than
+				expect(e).toBeInstanceOf(DatabaseConstraintFailError);
+				expect(service.createOne.mock.calls.length).toBe(1);
+				expect(service.createOne.mock.calls[0]).toEqual([req, entity]);
+			}
+		});
+
+		it('raise error throw pass, if any other error raise', async function () {
+			// given
+			const req = CRUDRequestMockBuilder.build();
+			const entity = new UserLikeEntity();
+
+			service.createOne.mockImplementation(() => {
+				throw new AnyOtherError();
+			});
+
+			//when
+			try {
+				await controller.createOne(req, entity);
+				expectShouldNotCallThis();
+			} catch (e) {
+				//than
+				expect(e).toBeInstanceOf(AnyOtherError);
+
+				expect(service.createOne.mock.calls.length).toBe(1);
+				expect(service.createOne.mock.calls[0]).toEqual([req, entity]);
+			}
+		});
+	});
+
+	describe('method deleteOne', () => {
+		it('should call this.base.deleteOne', async function () {
+			// given
+			const req = CRUDRequestMockBuilder.build();
+			const entity = new UserLikeEntity();
+
+			const stored = new UserLikeEntity();
+
+			service.deleteOne.mockImplementation(() => {
+				return stored;
+			});
+
+			//when
+			const result = await controller.deleteOne(req);
+
+			//than
+			expect(result).toEqual(stored);
+			expect(service.deleteOne.mock.calls.length).toBe(1);
+			expect(service.deleteOne.mock.calls[0]).toEqual([req]);
+		});
 	});
 });
