bc37561eea9804b69e08d6e92755384f02c22305
demetoir <wnsqlehlswk@naver.com>
2020-09-01 21:32:56 +0900

    feature(api/app): add entity.loader.ts

---
+++ api/src/database/global-typeorm/entity.loader.ts
@@ -0,0 +1,39 @@
+import { CommentEntity } from '../../comment/comment.entity';
+import { EditHistoryEntity } from '../../history/edit-history.entity';
+import { ImageEntity } from '../../image/image.entity';
+import { NoticeEntity } from '../../notice/notice.entity';
+import { RoleEntity } from '../../user/role/role.entity';
+import { ExpressionThumbnailImageEntity } from '../../expression/expression-thumbnail-image/expression-thumbnail-image.entity';
+import { ExpressionEntity } from '../../expression/expression/expression.entity';
+import { ExpressionSettingEntity } from '../../expression/expression-setting/expression-setting.entity';
+import { VectorEntity } from '../../vector/vector.entity';
+import { ScalarEntity } from '../../scalar/scalar.entity';
+import { TagEntity } from '../../tag/tag.entity';
+import { TeamEntity } from '../../team/team.entity';
+import { UserEntity } from '../../user/user/user.entity';
+import { UserLikeEntity } from '../../user/user-like/user-like.entity';
+import { UserSettingEntity } from '../../user/user-setting/user-setting.entity';
+import { UserOauthEntity } from '../../user/user-oauth/user-oauth.entity';
+import { UserProfileImageEntity } from '../../user/user-profile-image/user-profile-image.entity';
+
+const entities = [
+	CommentEntity,
+	EditHistoryEntity,
+	ImageEntity,
+	NoticeEntity,
+	RoleEntity,
+	ScalarEntity,
+	TagEntity,
+	TeamEntity,
+	UserEntity,
+	UserLikeEntity,
+	UserSettingEntity,
+	UserOauthEntity,
+	UserProfileImageEntity,
+	VectorEntity,
+	ExpressionEntity,
+	ExpressionSettingEntity,
+	ExpressionThumbnailImageEntity,
+];
+
+export { entities };
