a8b922046010fd9f7dbdf9ae7000756c3b7639ae
demetoir <wnsqlehlswk@naver.com>
2020-09-01 21:36:57 +0900

    feature(api/global-typeorm): add loadding options from .env

--- api/src/database/global-typeorm/global-typeorm.module.ts
+++ api/src/database/global-typeorm/global-typeorm.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config as ormConfig } from '../../common/model/configLoader';
+import { entities } from './entity.loader';
+import { configurationLoader } from '../../config/configurationLoader';
 
 @Module({
-	imports: [TypeOrmModule.forRoot(ormConfig)],
+	imports: [
+		TypeOrmModule.forRootAsync({
+			useFactory: async () => {
+				const config = configurationLoader();
+
+				return {
+					type: 'mysql',
+					host: config.TYPEORM_HOST,
+					port: config.TYPEORM_PORT,
+					database: config.TYPEORM_DATABASE,
+					username: config.TYPEORM_USERNAME,
+					password: config.TYPEORM_PASSWORD,
+					synchronize: config.TYPEORM_SYNCHRONIZE,
+					logging: config.TYPEORM_LOGGING,
+					bigNumberStrings: false,
+					entities: entities,
+					migrations: [],
+					subscribers: [],
+				};
+			},
+		}),
+	],
 })
 export class GlobalTypeormModule {}
