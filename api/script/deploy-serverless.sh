7fa061663da364f72690efdb62624307b13b80b4
demetoir <wnsqlehlswk@naver.com>
2020-09-04 23:30:32 +0900

    chore(api/serverless): add serverless deploy script

---
+++ api/script/deploy-serverless.sh
@@ -0,0 +1,19 @@
+pwd
+
+rm -rf .layer
+mkdir .layer
+
+cp ./package.json ./layer/package.json
+
+cd ./.layer
+npm install --production
+npm prune --production
+node-prune
+
+rm package.json
+rm package-lock.json
+
+cd ..
+
+yarn serverless deploy
+
