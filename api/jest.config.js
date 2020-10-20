b7870eef569d60b54f9b2cdf4bc97c04266d1567
demetoir <wnsqlehlswk@naver.com>
2020-10-20 23:08:03 +0900

    chore: able to absolute import while jest testing

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,16 +1,21 @@
 module.exports = {
-	globalSetup: './jest.setup.js',
-	globalTeardown: './jest.teardown.js',
-	transform: {
-		'^.+\\.tsx?$': 'ts-jest',
+	'moduleFileExtensions': [
+		'js',
+		'json',
+		'ts',
+	],
+	'moduleNameMapper': {
+		'^src/(.*)$': '<rootDir>/src/$1',
+		'^test/(.*)$': '<rootDir>/test/$1',
 	},
-	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	"moduleNameMapper": {
-		"^src/(.*)$": "<rootDir>/src/$1",
-		"^test/(.*)$": "<rootDir>/test/$1"
+	'rootDir': '.',
+	'testRegex': '.spec.ts$',
+	'transform': {
+		'^.+\\.(t|j)s$': 'ts-jest',
 	},
-	testTimeout: 30000,
-	maxConcurrency: 1,
-	maxWorkers: 1,
+	'coverageDirectory': '../coverage',
+	'testEnvironment': 'node',
+	'testTimeout': 30000,
+	'maxConcurrency': 1,
+	'maxWorkers': 1,
 };
