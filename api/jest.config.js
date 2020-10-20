2e59a1776342628a32f0ec2006bb12e456a31c5d
demetoir <wnsqlehlswk@naver.com>
2020-10-20 23:28:54 +0900

    chore: add missing jest config

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,21 +1,19 @@
 module.exports = {
-	'moduleFileExtensions': [
-		'js',
-		'json',
-		'ts',
-	],
-	'moduleNameMapper': {
+	moduleFileExtensions: ['js', 'json', 'ts'],
+	moduleNameMapper: {
 		'^src/(.*)$': '<rootDir>/src/$1',
 		'^test/(.*)$': '<rootDir>/test/$1',
 	},
-	'rootDir': '.',
-	'testRegex': '.spec.ts$',
-	'transform': {
+	rootDir: '.',
+	testRegex: '.spec.ts$',
+	transform: {
 		'^.+\\.(t|j)s$': 'ts-jest',
 	},
-	'coverageDirectory': '../coverage',
-	'testEnvironment': 'node',
-	'testTimeout': 30000,
-	'maxConcurrency': 1,
-	'maxWorkers': 1,
+	coverageDirectory: '../coverage',
+	testEnvironment: 'node',
+	testTimeout: 30000,
+	maxConcurrency: 1,
+	maxWorkers: 1,
+	globalSetup: './jest.setup.js',
+	globalTeardown: './jest.teardown.js',
 };
