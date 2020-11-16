d035f049ec6189dfe06b15839aa67cf7b2681985
demetoir <wnsqlehlswk@naver.com>
2020-11-16 23:01:09 +0900

    refactor(common/model): apply shortcut decorator to base.entity.ts

--- api/src/common/model/entity/base/base.entity.ts
+++ api/src/common/model/entity/base/base.entity.ts
@@ -1,31 +1,26 @@
-import {
-	CreateDateColumn,
-	DeleteDateColumn,
-	PrimaryGeneratedColumn,
-	UpdateDateColumn,
-} from 'typeorm';
-import tableIdType from '../../../libs/tableIdTypeResolver';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IBaseEntity } from './base.interface';
+import {
+	CreatedAtColumn,
+	DeletedAtColumn,
+	IdColumn,
+	UpdatedAtColumn,
+} from 'src/common/typeorm';
 
 export abstract class BaseEntity implements IBaseEntity {
 	@ApiPropertyOptional()
-	@PrimaryGeneratedColumn('increment', { type: tableIdType, name: 'id' })
+	@IdColumn()
 	id: number;
 
 	@ApiPropertyOptional()
-	@CreateDateColumn({ type: 'datetime', name: 'created_at', nullable: false })
+	@CreatedAtColumn()
 	createdAt: Date;
 
 	@ApiPropertyOptional()
-	@UpdateDateColumn({ type: 'datetime', name: 'updated_at', nullable: false })
+	@UpdatedAtColumn()
 	updatedAt: Date;
 
 	@ApiPropertyOptional()
-	@DeleteDateColumn({
-		type: 'datetime',
-		name: 'deleted_at',
-		nullable: true,
-	})
+	@DeletedAtColumn()
 	deletedAt: Date;
 }
