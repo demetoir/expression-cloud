6b0075806e30e1085ea08df0eb49c780bf71c4bc
demetoir <wnsqlehlswk@naver.com>
2020-11-22 18:22:04 +0900

    chore(api): apply airbnb typescript eslint style

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,24 +1,45 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	extends: [
+		'airbnb-typescript/base',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'prettier/@typescript-eslint',
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	rules: {
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-floating-promises': 'error',
+		'import/prefer-default-export': 'off',
+		'class-methods-use-this': 'off',
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				devDependencies: true,
+				optionalDependencies: false,
+				peerDependencies: false,
+			},
+		],
+		'padding-line-between-statements': [
+			'error',
+			{ blankLine: 'always', prev: '*', next: 'return' },
+			{ blankLine: 'always', prev: '*', next: 'if' },
+			{ blankLine: 'always', prev: '*', next: 'for' },
+			{ blankLine: 'always', prev: '*', next: 'try' },
+			{ blankLine: 'always', prev: '*', next: 'while' },
+			{ blankLine: 'always', prev: '*', next: 'break' },
+		],
+	},
 };
