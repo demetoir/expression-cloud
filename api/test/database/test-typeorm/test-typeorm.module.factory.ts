ab5bd375d5e809fdffd67b7e1d21ca400b4c10af
demetoir <wnsqlehlswk@naver.com>
2020-11-22 14:44:18 +0900

    test: add test-typeorm.module.factory.ts

---
+++ api/test/database/test-typeorm/test-typeorm.module.factory.ts
@@ -0,0 +1,33 @@
+import { DynamicModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { entities } from 'src/database/global-typeorm/entity.loader';
+import { configurationLoader } from 'src/config/configurationLoader';
+
+export const TestTypeormModuleFactory = (
+	database: string,
+	options?: any,
+): DynamicModule => {
+	return TypeOrmModule.forRootAsync({
+		imports: [ConfigModule],
+		useFactory: () => {
+			const config = configurationLoader();
+
+			return {
+				type: 'mysql',
+				host: config.TYPEORM_HOST,
+				port: config.TYPEORM_PORT,
+				database: database,
+				username: config.TYPEORM_USERNAME,
+				password: config.TYPEORM_PASSWORD,
+				synchronize: config.TYPEORM_SYNCHRONIZE,
+				logging: config.TYPEORM_LOGGING,
+				bigNumberStrings: false,
+				entities: entities,
+				migrations: [],
+				subscribers: [],
+				...options,
+			};
+		},
+	});
+};
