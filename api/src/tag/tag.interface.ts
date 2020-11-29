89c55f79d1f1778f79a0237dacbf1b42e2a78c10
demetoir <wnsqlehlswk@naver.com>
2020-11-29 20:25:37 +0900

    chore(api): remove useless interface

--- api/src/tag/tag.interface.ts
+++
@@ -1,8 +0,0 @@
-import { IBaseEntity } from '../common/model/entity/base/base.interface';
-import { IExpression } from '../expression/expression/expression.interface';
-
-export interface ITag extends IBaseEntity {
-	name: string;
-
-	expression: IExpression;
-}
--- api/src/user-setting/user-setting.interface.ts
+++
@@ -1,7 +0,0 @@
-import { IUser } from 'src/user/user.interface';
-import { IBaseEntity } from 'src/common/model/entity/base/base.interface';
-
-export declare interface IUserSetting extends IBaseEntity {
-	userId: number;
-	user: IUser;
-}
