f4e4cc750dc6633cc7b3cc8bf7a700b051b18354
demetoir <wnsqlehlswk@naver.com>
2020-11-30 21:02:10 +0900

    chore(user): delete pipe

--- api/src/user/pipe/userDtoTransfrom.pipe.ts
+++
@@ -1,4 +0,0 @@
-import { DTOTransformPipe } from 'src/common/pipe/dtoTransform.pipe';
-import { UserUpdateDto } from 'src/user/dto';
-
-export const dtoTransformPipe = new DTOTransformPipe(UserUpdateDto);
