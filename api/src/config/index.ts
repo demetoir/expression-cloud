eb64758bbbcde6fee3f78207307a06d78fd09e5f
demetoir <wnsqlehlswk@naver.com>
2020-12-21 21:40:37 +0900

    feat: add index to src/config

---
+++ api/src/config/index.ts
@@ -0,0 +1,5 @@
+export * from './configurationLoader';
+export * from './globalConfig.module';
+export * from './NodeConfig.service';
+export * from './openApiDocConfig.service';
+export * from './typeormConfig.service';
