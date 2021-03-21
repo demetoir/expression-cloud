178aef8983af79d1268d90db5af9fbe62a709995
demetoir <wnsqlehlswk@naver.com>
2021-03-21 22:47:41 +0900

    refactor: update index and simplify import path

--- api/src/core/user/model/index.ts
+++ api/src/core/user/model/index.ts
@@ -1,2 +1,6 @@
 export * from './user';
 export * from './role';
+export * from './user-setting';
+export * from './user-oauth';
+export * from './user-like';
+export * from './user-profile.image';
--- api/src/global/typeorm/entity.loader.ts
+++ api/src/global/typeorm/entity.loader.ts
@@ -1,7 +1,4 @@
-import { Expression } from 'src/core/equation/expression';
-import { Vector } from 'src/core/equation/vector';
-import { User } from '../../core';
-import { UserSetting } from '../../core/user/model/user-setting';
+import { Expression, User, UserSetting, Vector } from '../../core';
 
 const entities = [User, UserSetting, Expression, Vector];
 
