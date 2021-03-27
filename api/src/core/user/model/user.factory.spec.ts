cf2af3e2775e25796b941739435c7630b8a43619
demetoir <wnsqlehlswk@naver.com>
2021-03-27 20:43:01 +0900

    test: remove useless factory testing

--- api/src/core/user/model/user.factory.spec.ts
+++
@@ -1,9 +0,0 @@
-import { UserFactory } from './user.factory';
-
-describe('user factory', () => {
-	it('should build random user', async function () {
-		const user = UserFactory.build();
-
-		expect(user).toBeDefined();
-	});
-});
